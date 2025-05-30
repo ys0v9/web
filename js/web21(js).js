6c16a5bde994db1a9188b721011175ce2668ebf7
ys0v9 <sua9097@gmail.com>
2025-05-30 09:54:47 +0900

    js

---
+++ js/web21(js).js
@@ -0,0 +1,13 @@
+// let heading = document.getElementById('heading');
+//     heading.onclick = function() {
+//         heading.style.color = 'green';
+// }
+
+// alert("홈페이지에 오신 것을 환영합니다.");
+// confirm("정말 홈페이지에 들어가시겠습니까?");
+// prompt("정말 정말로 들어가시겠습니까?", "네");
+
+// document.writeln("<h1>안녕하세요</h1>")
+// document.writeln("안녕하세요");
+
+console.log("안녕하세요")

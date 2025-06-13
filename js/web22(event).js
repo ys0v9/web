ad3ded69215581cb334aff2eca70289894ab80c8
ys0v9 <sua9097@gmail.com>
2025-06-13 10:23:39 +0900

    web22(event)

---
+++ js/web22(event).js
@@ -0,0 +1,16 @@
+// function changebg(color) {
+//     // let box = document.getElementById('result');
+//     let box = document.querySelector('#result') //쿼리셀렉터는 모든 태그를 다 찾기 때문에 아이디는 #을 붙여줘야함
+//     box.style.backgroundColor = color;
+// }
+
+// let numbers = ["one", "two", "three", "four"]; // 배열 선언 및 초기화
+// for(let i = 0; i < numbers.length; i++) { // 배열의 각 요소에 접근하기
+//     console.log(numbers[i])
+// }
+
+// console.log(new Date('2024'));
+// console.log(new Date('2024-06'));
+// console.log(new Date('2024-02'));
+// console.log(new Date('2024-02-25T18:00:00'));
+// console.log(new Date('2024-02-25T18:00:00Z'));

597d922a3baf1baebf05d6e9f40afcbb589084d7
ys0v9 <sua9097@gmail.com>
2025-06-20 09:15:55 +0900

    web23(object)-당첨자 뽑기 추가

--- js/web23(object).js
+++ js/web23(object).js
@@ -1,9 +1,17 @@
-let start = new Date('2025-06-01');
-let now = new Date();
+// let start = new Date('2025-06-01');
+// let now = new Date();
 
-let pass_day = now.getTime() - start.getTime();
-pass_day = Math.round(pass_day / 1000 / 60 / 60 / 24);
+// let pass_day = now.getTime() - start.getTime();
+// pass_day = Math.round(pass_day / 1000 / 60 / 60 / 24);
 
-console.log(pass_day);
+// console.log(pass_day);
 
-document.getElementById('day').innerText = pass_day;
+// document.getElementById('day').innerText = pass_day;
+
+
+//당첨자 뽑기
+let seed = prompt("전체 응모자 수");
+let picked = Math.floor((Math.random() * seed) + 1);
+
+document.getElementById("total").textContent = `전체 응모자수: ${seed}명`;
+document.getElementById("winner").textContent = `당첨자: ${picked}번`;
